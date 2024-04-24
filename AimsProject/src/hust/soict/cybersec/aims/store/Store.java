caea8e852728f4c12e4d245fb0d894aa89641e0c
jason1303 <hainhat365@gmail.com>
2024-04-24 15:25:26 +0700

    implement store clas

--- AimsProject/src/hust/soict/cybersec/aims/store/Store.java
+++ AimsProject/src/hust/soict/cybersec/aims/store/Store.java
@@ -1,5 +1,58 @@
 package hust.soict.cybersec.aims.store;
 
-public class Store {
+import hust.soict.cybersec.aims.disc.DigitalVideoDisc;
 
+public class Store {
+	
+	private int qtyitemsInStore;
+	private DigitalVideoDisc itemsInStore[];
+	private int maxstore ;
+
+	//constructor
+	public Store() {
+		qtyitemsInStore = 0;
+		itemsInStore = new DigitalVideoDisc[100];
+		maxstore = 100;
+	}
+	public Store(int maxstore) {
+		qtyitemsInStore = 0;
+		itemsInStore = new DigitalVideoDisc[maxstore];
+		this.maxstore = maxstore;
+	}
+	
+	
+	public void addDVD (DigitalVideoDisc dvd) {
+		if (qtyitemsInStore == maxstore) {
+			System.out.println("The store is full");
+			return;
+		}else {
+			
+			itemsInStore[qtyitemsInStore] = dvd;
+			System.out.println("This DVD is added to store successfully");
+			qtyitemsInStore +=1;
+		}
+		
+	}
+	
+	public void removeDVD (DigitalVideoDisc dvd) {
+		int index = -1;
+		for (int i = 0 ; i < maxstore ; i ++) {
+			if(itemsInStore[i] == dvd) {
+				
+				index = i;
+				break;
+			}
+		}
+		if (index == -1) {
+			System.out.println("This DVD is not found in the store");
+			
+		}else {
+			qtyitemsInStore -= 1 ;
+			System.out.println("This DVD is remove successfully");
+			for (int i = index ; i < maxstore - 1; i++) {
+				itemsInStore[i] = itemsInStore[i+1];	
+			}
+			itemsInStore[maxstore -1 ] = null;
+		}
+	}
 }
--- AimsProject/src/hust/soict/cybersec/aims/store/StoreTest.java
+++ AimsProject/src/hust/soict/cybersec/aims/store/StoreTest.java
@@ -1,5 +1,22 @@
 package hust.soict.cybersec.aims.store;
 
-public class StoreTest {
+import hust.soict.cybersec.aims.disc.DigitalVideoDisc;
 
+public class StoreTest {
+	public static void main(String[] args) {
+		
+		Store store = new Store(2);
+		
+		DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King",
+				"Animation", "Roger Allers", 87,19.95f);
+		
+		
+		DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star War", 
+				"Science Fiction", "George Lucas", 87, 24.95f);
+		
+		store.addDVD(dvd2);
+		store.addDVD(dvd1);
+		
+		store.removeDVD(dvd2);
+	}
 }
